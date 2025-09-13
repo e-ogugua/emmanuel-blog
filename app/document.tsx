8a4f0919dca3210bea2512e92dfb7f679137411a
Emmanuel Ogugua <emmachuka@gmail.com>
2025-09-13 10:08:32 +0100

    fix: add custom _document.tsx for proper CSS loading

---
+++ app/document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="/_next/static/css/8e4deb4099dd767d.css"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          href="/_next/static/css/8e4deb4099dd767d.css"
+          as="style"
+        />
+        <link
+          rel="preload"
+          href="/_next/static/css/cbbe1a66cef0a72d.css"
+          as="style"
+        />
+        <link
+          rel="stylesheet"
+          href="/_next/static/css/cbbe1a66cef0a72d.css"
+          as="style"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
